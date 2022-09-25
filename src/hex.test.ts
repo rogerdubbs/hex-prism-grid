5a86a807ee3bf3c203f608e56de412d5ab01ead4
rogerdubbs <roger_dubbs@yahoo.com>
2022-09-25 07:56:54 -0700

    Make a real initial test

--- src/hex.test.ts
+++ src/hex.test.ts
@@ -1,8 +1,29 @@
 import { Hex, HexType } from "./hex";
 
-test("A pointy hexagon of size 10 can be created", () => {
+describe("A pointy hexagon of size 10", () => {
   const hex = new Hex({ x: 0, y: 0 }, HexType.PointyTopped, 10);
-  expect(hex.center).toStrictEqual({ x: 0, y: 0 });
-  expect(hex.type).toBe(HexType.PointyTopped);
-  expect(hex.size).toBe(10);
+  const halfWidth = (10 * Math.sqrt(3)) / 2;
+  it("is centered at the designated spot", () => {
+    expect(hex.center).toStrictEqual({ x: 0, y: 0 });
+  });
+  it("is recorded as pointy-topped", () => {
+    expect(hex.type).toBe(HexType.PointyTopped);
+  });
+  it("has the correct size", () => {
+    expect(hex.type).toBe(HexType.PointyTopped);
+  });
+  it("has the correct points", () => {
+    expect(hex.corner(0).x).toBeCloseTo(halfWidth);
+    expect(hex.corner(0).y).toBeCloseTo(-5);
+    expect(hex.corner(1).x).toBeCloseTo(halfWidth);
+    expect(hex.corner(1).y).toBeCloseTo(5);
+    expect(hex.corner(2).x).toBeCloseTo(0);
+    expect(hex.corner(2).y).toBeCloseTo(10);
+    expect(hex.corner(3).x).toBeCloseTo(-halfWidth);
+    expect(hex.corner(3).y).toBeCloseTo(5);
+    expect(hex.corner(4).x).toBeCloseTo(-halfWidth);
+    expect(hex.corner(4).y).toBeCloseTo(-5);
+    expect(hex.corner(5).x).toBeCloseTo(0);
+    expect(hex.corner(5).y).toBeCloseTo(-10);
+  });
 });
--- src/hex.ts
+++ src/hex.ts
@@ -1,10 +1,25 @@
+export interface Point {
+  x: number;
+  y: number;
+}
+
 export enum HexType {
   PointyTopped,
 }
+
 export class Hex {
   constructor(
-    public center: { x: number; y: number },
+    public center: Point,
     public type: HexType,
     public size: number
   ) {}
+
+  corner(index: number): Point {
+    const angle_deg = 60 * index - 30;
+    const angle_rad = (Math.PI / 180) * angle_deg;
+    return {
+      x: this.center.x + this.size * Math.cos(angle_rad),
+      y: this.center.y + this.size * Math.sin(angle_rad),
+    };
+  }
 }
