3cac1424b201db718c195c3959049182fdd03bc3
rogerdubbs <roger_dubbs@yahoo.com>
2022-09-25 08:56:49 -0700

    Add a bunch of properties of the hexagon, with tests.  Rewrite the test for points to be in terms of those properties.  Add support for flat-topped hexagons.

--- src/hex.test.ts
+++ src/hex.test.ts
@@ -2,7 +2,6 @@ import { Hex, HexType } from "./hex";
 
 describe("A pointy hexagon of size 10", () => {
   const hex = new Hex({ x: 0, y: 0 }, HexType.PointyTopped, 10);
-  const halfWidth = (10 * Math.sqrt(3)) / 2;
   it("is centered at the designated spot", () => {
     expect(hex.center).toStrictEqual({ x: 0, y: 0 });
   });
@@ -10,20 +9,83 @@ describe("A pointy hexagon of size 10", () => {
     expect(hex.type).toBe(HexType.PointyTopped);
   });
   it("has the correct size", () => {
-    expect(hex.type).toBe(HexType.PointyTopped);
+    expect(hex.size).toBe(10);
   });
-  it("has the correct points", () => {
-    expect(hex.corner(0).x).toBeCloseTo(halfWidth);
-    expect(hex.corner(0).y).toBeCloseTo(-5);
-    expect(hex.corner(1).x).toBeCloseTo(halfWidth);
-    expect(hex.corner(1).y).toBeCloseTo(5);
+  it("has an inradius of 8.66", () => {
+    expect(hex.inradius).toBeCloseTo(8.66);
+  });
+  it("has a maximalDiameter of 20", () => {
+    expect(hex.maximalDiameter).toBeCloseTo(20);
+  });
+  it("has a minimalDiameter of 17.32", () => {
+    expect(hex.minimalDiameter).toBeCloseTo(17.32);
+  });
+  it("has the same circumradius as size", () => {
+    expect(hex.circumradius).toBe(hex.size);
+  });
+  it("has a width equal to the minimalDiameter", () => {
+    expect(hex.width).toBeCloseTo(hex.minimalDiameter);
+  });
+  it("has a height equal to the maximalDiameter", () => {
+    expect(hex.height).toBeCloseTo(hex.maximalDiameter);
+  });
+  it("has the correct corners", () => {
+    expect(hex.corner(0).x).toBeCloseTo(hex.width / 2);
+    expect(hex.corner(0).y).toBeCloseTo(-hex.height / 4);
+    expect(hex.corner(1).x).toBeCloseTo(hex.width / 2);
+    expect(hex.corner(1).y).toBeCloseTo(hex.height / 4);
     expect(hex.corner(2).x).toBeCloseTo(0);
-    expect(hex.corner(2).y).toBeCloseTo(10);
-    expect(hex.corner(3).x).toBeCloseTo(-halfWidth);
-    expect(hex.corner(3).y).toBeCloseTo(5);
-    expect(hex.corner(4).x).toBeCloseTo(-halfWidth);
-    expect(hex.corner(4).y).toBeCloseTo(-5);
+    expect(hex.corner(2).y).toBeCloseTo(hex.height / 2);
+    expect(hex.corner(3).x).toBeCloseTo(-hex.width / 2);
+    expect(hex.corner(3).y).toBeCloseTo(hex.height / 4);
+    expect(hex.corner(4).x).toBeCloseTo(-hex.width / 2);
+    expect(hex.corner(4).y).toBeCloseTo(-hex.height / 4);
     expect(hex.corner(5).x).toBeCloseTo(0);
-    expect(hex.corner(5).y).toBeCloseTo(-10);
+    expect(hex.corner(5).y).toBeCloseTo(-hex.height / 2);
+  });
+});
+
+describe("A flat topped hexagon of size 10", () => {
+  const hex = new Hex({ x: 0, y: 0 }, HexType.FlatTopped, 10);
+  it("is centered at the designated spot", () => {
+    expect(hex.center).toStrictEqual({ x: 0, y: 0 });
+  });
+  it("is recorded as flat-topped", () => {
+    expect(hex.type).toBe(HexType.FlatTopped);
+  });
+  it("has the correct size", () => {
+    expect(hex.size).toBe(10);
+  });
+  it("has the same circumradius as size", () => {
+    expect(hex.circumradius).toBe(hex.size);
+  });
+  it("has a width equal to the maximalDiameter", () => {
+    expect(hex.width).toBeCloseTo(hex.maximalDiameter);
+  });
+  it("has an inradius of 8.66", () => {
+    expect(hex.inradius).toBeCloseTo(8.66);
+  });
+  it("has a height equal to the minimalDiameter", () => {
+    expect(hex.height).toBeCloseTo(hex.minimalDiameter);
+  });
+  it("has a maximalDiameter of 20", () => {
+    expect(hex.maximalDiameter).toBeCloseTo(20);
+  });
+  it("has a minimalDiameter of 17.32", () => {
+    expect(hex.minimalDiameter).toBeCloseTo(17.32);
+  });
+  it("has the correct corners", () => {
+    expect(hex.corner(0).x).toBeCloseTo(hex.width / 2);
+    expect(hex.corner(0).y).toBeCloseTo(0);
+    expect(hex.corner(1).x).toBeCloseTo(hex.width / 4);
+    expect(hex.corner(1).y).toBeCloseTo(hex.height / 2);
+    expect(hex.corner(2).x).toBeCloseTo(-hex.width / 4);
+    expect(hex.corner(2).y).toBeCloseTo(hex.height / 2);
+    expect(hex.corner(3).x).toBeCloseTo(-hex.width / 2);
+    expect(hex.corner(3).y).toBeCloseTo(0);
+    expect(hex.corner(4).x).toBeCloseTo(-hex.width / 4);
+    expect(hex.corner(4).y).toBeCloseTo(-hex.height / 2);
+    expect(hex.corner(5).x).toBeCloseTo(hex.width / 4);
+    expect(hex.corner(5).y).toBeCloseTo(-hex.height / 2);
   });
 });
--- src/hex.ts
+++ src/hex.ts
@@ -5,7 +5,10 @@ export interface Point {
 
 export enum HexType {
   PointyTopped,
+  FlatTopped,
 }
+
+const sqrt3 = Math.sqrt(3);
 
 export class Hex {
   constructor(
@@ -15,11 +18,43 @@ export class Hex {
   ) {}
 
   corner(index: number): Point {
-    const angle_deg = 60 * index - 30;
+    const angle_deg =
+      60 * index - (this.type === HexType.PointyTopped ? 30 : 0);
     const angle_rad = (Math.PI / 180) * angle_deg;
     return {
       x: this.center.x + this.size * Math.cos(angle_rad),
       y: this.center.y + this.size * Math.sin(angle_rad),
     };
+  }
+
+  get height(): number {
+    if (this.type === HexType.PointyTopped) {
+      return this.maximalDiameter;
+    } else {
+      return this.minimalDiameter;
+    }
+  }
+
+  get circumradius(): number {
+    return this.size;
+  }
+
+  get inradius(): number {
+    return (this.size * sqrt3) / 2;
+  }
+
+  get width(): number {
+    if (this.type === HexType.PointyTopped) {
+      return this.minimalDiameter;
+    } else {
+      return this.maximalDiameter;
+    }
+  }
+
+  get maximalDiameter(): number {
+    return this.size * 2;
+  }
+  get minimalDiameter(): number {
+    return 2 * this.inradius;
   }
 }
